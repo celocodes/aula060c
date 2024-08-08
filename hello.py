faccc6b2e30f4f5f96b0d59d2f2f8846d9bf009a
cel01_ <102827591+celocodes@users.noreply.github.com>
2024-08-08 20:29:48 -0300

    aaaa

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -21,6 +21,7 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+ROLES = [('User', 'User'), ('Administrator', 'Administrator'), ('Moderator', 'Moderator')]
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -31,7 +32,6 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -41,35 +41,29 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    role = SelectField('Role?', choices=[('Administrator', 'Administrator'), ('Moderator', 'Moderator'), ('User', 'User')])
+    role = SelectField('Role?', choices=ROLES, validators=[DataRequired()])
     submit = SubmitField('Submit')
-
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-    user_all = User.query.all();
-    role_all = Role.query.all();
-    print(user_all);
-    print(role_all);
+    user_all = User.query.all()
+    roles = Role.query.all()
+    
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
@@ -90,3 +84,6 @@ def index():
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False),
                            user_all=user_all, roles=roles)
+
+if __name__ == '__main__':
+    app.run(debug=True)
